74a859c134f4c144fce5d549eee4db147eddb7be
Rupert Lester <rlester@barweb.com.au>
2016-01-20 10:40:28 +1000

    no message

--- AppVeyorTest/Tests.cs
+++ AppVeyorTest/Tests.cs
@@ -23,7 +23,7 @@ namespace AppVeyorTest
         public void HelloIsHello()
         {
             var client = new FlurlClient();
-            client.Url = "http://localhost:80/test";
+            client.Url = "http://localhost:80/Default%20Web%20%Site/test";
             var response = client.GetAsync();
             response.Result.StatusCode.Should().Be(HttpStatusCode.OK);
 
