5433e1867c5d5bbc6efc847e9f4aa9e4017d4d89
McKay Heares <mheares@orlitech.local>
2016-01-21 09:21:37 +1000

    no message

--- MakeSQLHappen.bat
+++ MakeSQLHappen.bat
@@ -1,1 +1,1 @@
-"C:\\Program Files\\MySQL\\MySQL Server 5.6\\bin\\mysql.exe" --verbose --force --user=root --password=Password12! < "C:\Users\mheares\Desktop\sql.sql"
+"C:\\Program Files\\MySQL\\MySQL Server 5.6\\bin\\mysql.exe" --verbose --force --user=root --password=Password12! < "C:\projects\appveyortestproject\sql.sql"
