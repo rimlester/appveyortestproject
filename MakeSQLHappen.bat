01c7036601dee563578a2b5960dc97474fa288a7
Rupert Lester <rlester@barweb.com.au>
2016-01-21 11:04:14 +1000

    mouth fedora

--- MakeSQLHappen.bat
+++ MakeSQLHappen.bat
@@ -1,3 +1,1 @@
-"C:\\Program Files\\MySQL\\MySQL Server 5.6\\bin\\mysqld.exe"
-echo "Hello"
 "C:\\Program Files\\MySQL\\MySQL Server 5.6\\bin\\mysql.exe" --verbose --force --user=root --password=Password12! < "C:\projects\appveyortestproject\sql.sql"
