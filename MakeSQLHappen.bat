adff065019519c24a2e91787713798638f57bde6
Rupert Lester <rlester@barweb.com.au>
2016-01-21 10:58:45 +1000

    invoking mysqld seems to hang

--- MakeSQLHappen.bat
+++ MakeSQLHappen.bat
@@ -1,2 +1,3 @@
 "C:\\Program Files\\MySQL\\MySQL Server 5.6\\bin\\mysqld.exe"
+echo "Hello"
 "C:\\Program Files\\MySQL\\MySQL Server 5.6\\bin\\mysql.exe" --verbose --force --user=root --password=Password12! < "C:\projects\appveyortestproject\sql.sql"
